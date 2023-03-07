d89257013110cb82d8a4275e74e74ed3be025db9
Ray Shah <59604100+gitfrog0@users.noreply.github.com>
2023-03-07 18:35:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+GKE CFK Load balancer cleanup scripts
+===================
+
+This script will remove all load balancers created by Confluent for Kubernetes for a GKE cluster.
+When configuring a external load balancer in CFK, it will create a forwarding rule, a target pool, and a health check.
+If the GKE cluster is deleted without first removing the load balancers, they become orphaned.
+The script will remove the forwarding rule, target pool, and health check.
+
+<!-- toc -->
+
+- [Usage](#usage)
+  * [Configuration](#configuration)
+  * [Running the script](#running-the-script)
+
+<!-- tocstop -->
+
+Usage
+-----
+
+### Configuration
+
+Configuration is handled through environment variables:
+
+- `PROJECT`: The GCE project that we should operate on
+- `REGION`: The region where GCE resources should be probed
+- `GKE_CLUSTER_NAME`: The Kube cluster name for verifying network resources against
+- `DRYRUN`: Run script without executing deletes
+
+### Running the script
+
+Set the env variables and execute the script
+```
+PROJECT=myproject \
+REGION=us-central1 \
+GKE_CLUSTER_NAME=cfk-mycluster \
+./gke-cfk-lb-cleanup.sh
+```
+
+This script interates through gcloud target pools for the provided cluster, 
+identifying which are no longer associated with valid instances.
---
+++ gke-cfk-lb-cleanup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+delete_gce_lb_objects() {
+    local id="$1"
+    local healthcheckname=""
+
+    # lookup healthcheck name
+    healthchecklink=$(gcloud compute --project="${PROJECT}" target-pools describe "${id}" --format='json' | jq -r '.healthChecks' | jq -r '.[0]')
+
+    # check if healthcheck covers multiple target pools, if only 1 target pool, we can delete it
+    echo "LOOKUP healthcheck ${healthchecklink}"
+    targetpoolcount=$(gcloud compute --project="${PROJECT}" target-pools list --format='json' --filter="region:( ${REGION} )" --filter="healthChecks:( ${healthchecklink} )" | jq length)
+    if [[ $targetpoolcount == 1 ]] ; then
+        healthcheckname=$(gcloud --project="${PROJECT}" compute http-health-checks list --filter="selfLink: ( ${healthchecklink} )" --format="json" | jq -r '.[]' | jq -r '.name')
+    fi
+
+    echo "DELETING forwarding-rule ${id}"
+    if [[ -z "$DRYRUN" ]] ; then 
+        gcloud compute --project="${PROJECT}" forwarding-rules delete "${id}" --region=${REGION} --quiet
+    fi
+    echo "DELETING target-pool ${id}"
+    if [[ -z "$DRYRUN" ]] ; then
+        gcloud compute --project="${PROJECT}" target-pools     delete "${id}" --region=${REGION} --quiet
+    fi
+
+    if [[ ! -z $healthcheckname ]] ; then
+        echo "DELETING healthcheck ${healthcheckname}"
+        if [[ -z "$DRYRUN" ]] ; then
+            gcloud compute --project="${PROJECT}" http-health-checks delete "${healthcheckname}" --quiet
+        fi
+    fi
+}
+
+# lookup all target pools for provided cluster
+for target in $(gcloud compute --project="${PROJECT}" target-pools list --format='json' --filter="region:( ${REGION} )" --filter="instances:( gke-${GKE_CLUSTER_NAME} )" | jq -r '.[] ' | jq -r '.name') ; do
+    # collect the target pools that don't have healthy instances
+    if ! $(gcloud compute --project="${PROJECT}" target-pools get-health "${target}" --region="${REGION}" 2>/dev/null >/dev/null); then
+        delete_gce_lb_objects "$target"
+        break
+    fi
+done
